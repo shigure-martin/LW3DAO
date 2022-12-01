216558265db9aca157362206876c3539d0880002
ShigureMartin <363354891@qq.com>
2022-12-01 21:54:56 +0800

    init commit

---
+++ contracts/CryptoDevsDAO.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+interface IFakeNFTMarkeplace {
+    function getPrice() external view returns (uint256);
+    function available(uint256 _tokenId) external view returns (bool);
+    function purchase(uint256 _tokenId) external payable;
+}
+
+interface ICryptoDevsNFT {
+    function balanceOf(address owner) external view returns (uint256);
+
+    function tokenOfOwnerByIndex(address owner, uint256 index) external view returns (uint256);
+}
+
+contract CryptoDevsDAO is Ownable {
+
+    struct Proposal {
+        uint256 nftTokenID;
+        uint256 deadline;
+        uint256 yayVotes;
+        uint256 nayVotes;
+        bool executed;
+        mapping(uint256 => bool)voters;
+    }
+
+    mapping(uint256 => Proposal) public proposals;
+
+    uint256 public numProposals;
+
+}
+
