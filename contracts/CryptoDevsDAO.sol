660b3e40bc2bfeb7e79df21c44a5d61b5801d560
ShigureMartin <363354891@qq.com>
2022-12-07 13:37:29 +0800

    contract dev complete

--- contracts/CryptoDevsDAO.sol
+++ contracts/CryptoDevsDAO.sol
@@ -18,7 +18,7 @@ interface ICryptoDevsNFT {
 contract CryptoDevsDAO is Ownable {
 
     struct Proposal {
-        uint256 nftTokenID;
+        uint256 nftTokenId;
         uint256 deadline;
         uint256 yayVotes;
         uint256 nayVotes;
@@ -30,5 +30,86 @@ contract CryptoDevsDAO is Ownable {
 
     uint256 public numProposals;
 
+    IFakeNFTMarkeplace nftMarketplace;
+    ICryptoDevsNFT cryptoDevsNFT;
+
+    enum Vote {
+        YAY,
+        NAY
+    }
+
+    constructor(address _nftMarketplace, address _cryptoDevsNFT) payable {
+        nftMarketplace = IFakeNFTMarkeplace(_nftMarketplace);
+        cryptoDevsNFT = ICryptoDevsNFT(_cryptoDevsNFT);
+    }
+
+    modifier nftHolderOnly() {
+        require(cryptoDevsNFT.balanceOf(msg.sender) > 0, "NOT_A_DAO_MEMBER");
+        _;
+    }
+
+    modifier activeProposalOnly(uint256 proposalIndex) {
+        require(proposals[proposalIndex].deadline > block.timestamp, "DEADLINE_EXCEEDED");
+        _;
+    }
+
+    modifier inactiveProposalOnly(uint256 proposalIndex) {
+        require(proposals[proposalIndex].deadline <= block.timestamp, "DEADLINE_NOT_EXCEEDED");
+        require(proposals[proposalIndex].executed == false, "PROPOSAL_ALREADY_EXECUTED");
+        _;
+    }
+
+    function createProposal(uint256 _nftTokenId) external nftHolderOnly returns (uint256) {
+        require(nftMarketplace.available(_nftTokenId), "NFT_NOT_FOR_SALE");
+        Proposal storage proposal = proposals[numProposals];
+        proposal.nftTokenId = _nftTokenId;
+
+        proposal.deadline = block.timestamp + 5 minutes;
+
+        numProposals++;
+
+        return numProposals - 1;
+    }
+
+    function voteOnProposal(uint256 proposalIndex, Vote vote) external activeProposalOnly(proposalIndex) {
+        Proposal storage proposal = proposals[proposalIndex];
+
+        uint256 voterNFTBalance = cryptoDevsNFT.balanceOf(msg.sender);
+        uint256 numVotes = 0;
+
+        for (uint256 i = 0; i < voterNFTBalance; i++) {
+            uint256 tokenId = cryptoDevsNFT.tokenOfOwnerByIndex(msg.sender, i);
+            if (proposal.voters[tokenId] == false) {
+                numVotes++;
+                proposal.voters[tokenId] == true;
+            }
+        }
+        require(numVotes > 0, "ALREADY_VOTED");
+
+        if (vote == Vote.YAY) {
+            proposal.yayVotes += numVotes;
+        } else {
+            proposal.nayVotes += numVotes;
+        }
+    }
+
+    function executeProposal(uint256 proposalIndex) external nftHolderOnly inactiveProposalOnly(proposalIndex) {
+        Proposal storage proposal = proposals[proposalIndex];
+
+        if(proposal.yayVotes > proposal.nayVotes) {
+            uint256 nftPrice = nftMarketplace.getPrice();
+            require(address(this).balance >= nftPrice, "NOT_ENOUGH_FUNDS");
+            nftMarketplace.purchase{value: nftPrice}(proposal.nftTokenId);
+        }
+        proposal.executed = true;
+    }
+
+    function withdrawEther() external onlyOwner {
+        payable(owner()).transfer(address(this).balance);
+    }
+
+    receive() external payable {}
+
+    fallback() external payable {}
 }
 
